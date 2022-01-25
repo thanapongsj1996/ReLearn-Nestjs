b9845ec82c334f8e496899b72678e91922a0a695
thanapongsj <thanapongsj1996@gmail.com>
2022-01-26 00:56:57 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule)
   app.use(cookieParser())
   app.setGlobalPrefix("/api/v1")
-  app.enableCors({ credentials: true })
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
 
   await app.listen(8000)
 }
