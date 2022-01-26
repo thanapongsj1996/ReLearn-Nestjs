2ad7334e0b12b03d40cb3229bf4bea90ee214917
thanapongsj <thanapongsj1996@gmail.com>
2022-01-26 21:19:16 +0700

    add origin

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(cookieParser())
   app.setGlobalPrefix("/api/v1")
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', process.env.FRONTEND_URL],
     credentials: true
   })
 
