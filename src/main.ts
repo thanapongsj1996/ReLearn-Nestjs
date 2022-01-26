b9ec1b9161c5c581a54f91b1ffb419383f3094c1
thanapongsj <thanapongsj1996@gmail.com>
2022-01-26 22:14:08 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,11 @@ import { AppModule } from './modules/app/app.module'
 dotenv.config()
 
 async function bootstrap() {
-  const frontendUrl = process.env.FRONTEND_URL
-  const whitelist = [frontendUrl, 'http://localhost:3000'];
-
   const app = await NestFactory.create(AppModule)
   app.use(cookieParser())
   app.setGlobalPrefix("/api/v1")
   app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log("allowed cors for:", origin)
-        callback(null, true)
-      } else {
-        console.log("blocked cors for:", origin)
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
     credentials: true
   })
 
